c359a008fcfbf14ed38a711e3a350ff773cc54a6
acostanahuel <acostanahuel@gmail.com>
2024-01-05 20:36:37 -0300

    tasks

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
+
 def add(a: float, b: float) -> float:
-    """
-    Adds two float numbers and returns their sum.
-    """
-    
+    """Adds two float numbers and returns their sum"""
     return a + b 
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Task 3 """
+
+def to_str(n: float) -> str:
+    """to string function"""
+    return str(n)
