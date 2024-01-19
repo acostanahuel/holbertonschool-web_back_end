5f3dc3f6d246152168084968d3e41b18e1b07f38
acostanahuel <acostanahuel17@gmail.com>
2024-01-18 21:00:23 -0300

    task 0

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+function taskFirst(){
+    const task = 'I prefer const when I can.';
+    return task;
+}
+
+export function getLast() {
+    return ' is okay';
+  }
+  
+  export function taskNext() {
+    var combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
