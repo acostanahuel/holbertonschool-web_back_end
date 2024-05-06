4f6b1b2e32a85d1a04dbf459ff9ca62a102faa1d
acostanahuel <acostanahuel@gmail.com>
2024-05-06 13:14:24 -0300

    first commit

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const nArr = [];
-  
-  for (const idx of array) {
+
+for (const idx of array) {
     nArr.push(appendString + idx);
   }
   
---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,58 @@
+xport default class HolbertonCourse {
+    constructor(name, length, students) {
+      
+      if (typeof (name) === 'string') {
+        this.__name = name;
+      } else {
+        throw new TypeError('Name must be a string');
+      }
+  
+      if (typeof (length) === 'number') {
+        this.__length = length;
+      } else {
+        throw new TypeError('Length must be a number');
+      }
+  
+      if (students.every((i) => typeof i === 'string')) {
+        this.__students = students;
+      } else {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
+  
+    get name() {
+      return this.__name;
+    }
+  
+    set name(name) {
+      if (typeof (name) === 'string') {
+        this.__name = name;
+      } else {
+        throw new TypeError('Name must be a string');
+      }
+    }
+  
+    get length() {
+      return this.__length;
+    }
+  
+    set length(l) {
+      if (typeof (l) === 'number') {
+        this.__length = l;
+      } else {
+        throw new TypeError('Length must be a number');
+      }
+    }
+  
+    get students() {
+      return this.__students;
+    }
+  
+    set students(std) {
+      if (std.every((i) => typeof i === 'string')) {
+        this.__students = std;
+      } else {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
+}
